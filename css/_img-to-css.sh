e82020eb6cd1cf05e7e62645a8a1372a1eae586d
Jarrell Waggoner <malloc47@gmail.com>
2012-03-17 14:01:54 -0400

    new icons and script now handles images of different sizes

--- css/_img-to-css.sh
+++ css/_img-to-css.sh
@@ -2,8 +2,8 @@
 echo "" > $2
 for i in `ls ../img/*$1.png` ; do
 	echo -n ".$(basename ${i%$1.png}) {
-	width:25px;
-	height:25px;
+	width:$(identify $i | awk '{print $3}' | awk -Fx '{print $1}')px;
+	height:$(identify $i | awk '{print $3}' | awk -Fx '{print $2}')px;
 	background-repeat: no-repeat;	
 	background-image: url(data:image/png;base64," >> $2
 	../img/_base64.sh $i >> $2
