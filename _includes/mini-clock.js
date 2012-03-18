b2188f4b32347362b8fb1615c8160dd474d63df2
Jarrell Waggoner <malloc47@gmail.com>
2012-03-18 14:15:33 -0400

    cleaned up a little code--much more left

--- _includes/mini-clock.js
+++ _includes/mini-clock.js
@@ -4,14 +4,13 @@ function drawClock() {
     var d = new Date();
 
     // Swap from day to night or night to day
-    if((d.getHours() < 19 && 
+    if ((d.getHours() < 19 && 
 	d.getHours() > 6) && 
        !window.theme && 
        !window.theme_override) {
 	check_time();
 	return;
-    }
-    else if(!(d.getHours() < 19 && 
+    } else if (!(d.getHours() < 19 && 
 	      d.getHours() > 6) && 
 	    window.theme &&
 	    !window.theme_override) {
@@ -21,38 +20,37 @@ function drawClock() {
 
     // Draw clock
     var e = document.getElementById('miniclock');
-    if(e && e.getContext) {
+    if (e && e.getContext) {
 	var c = e.getContext('2d');
 	var w = e.width;
 	var h = e.height;
-	c.clearRect(0,0,w,h);
+	c.clearRect(0, 0, w, h);
 
 
-	if(d.getHours()>11) {
+	if (d.getHours()>11) {
 	    // afternoon
-	    drawPie(c,w/2,h/2,w/2,hr(12),hr(7),colors.dark);
-	    drawPie(c,w/2,h/2,w/2,hr(7),hr(12),colors.light);
-	}
-	else {
+	    drawPie(c, w/2, h/2, w/2, hr(12), hr(7),  colors.dark);
+	    drawPie(c, w/2, h/2, w/2, hr(7),  hr(12), colors.light);
+	} else {
 	    // morning
-	    drawPie(c,w/2,h/2,w/2,hr(12),hr(6.5),colors.light);
-	    drawPie(c,w/2,h/2,w/2,hr(6.5),hr(12),colors.dark);
+	    drawPie(c, w/2, h/2, w/2, hr(12),  hr(6.5), colors.light);
+	    drawPie(c, w/2, h/2, w/2, hr(6.5), hr(12),  colors.dark);
 	}
 
 	// 43200 = (24*60*60)/2
 	var p = ((d.getHours()*3600+d.getMinutes()*60+d.getSeconds())%43200.0)/43200.0;
 
-	drawLine(c,w/2,h/2,w/3,cp(p),w/66,colors.hour);
-	drawLine(c,w/2,h/2,w/2.25,cp(d.getMinutes()/60),w/66,colors.minute);
-	drawLine(c,w/2,h/2,w/2,cp(d.getSeconds()/60),w/100,colors.second);
+	drawLine(c, w/2, h/2, w/3, cp(p), w/66, colors.hour);
+	drawLine(c, w/2, h/2, w/2.25, cp(d.getMinutes()/60), w/66, colors.minute);
+	drawLine(c, w/2, h/2, w/2, cp(d.getSeconds()/60), w/100, colors.second);
 
 	// circle to cover up the center
-	drawCircle(c,w/2,h/2,w/25,colors.center);
+	drawCircle(c, w/2, h/2, w/25, colors.center);
 	setTimeout(function (){drawClock();},500);
     }
 }
 
-function drawCircle(c,p1,p2,r,clr) {
+function drawCircle(c, p1, p2, r, clr) {
     c.save();
     c.fillStyle = clr;
     c.beginPath();
@@ -62,7 +60,7 @@ function drawCircle(c,p1,p2,r,clr) {
     c.restore();
 }
 
-function drawPie(c,p1,p2,r,a1,a2,clr) {
+function drawPie(c, p1, p2, r, a1, a2, clr) {
     // convert into standard unit circle coords
     var r1 = (Math.PI/180)*(360-a1);
     var r2 = (Math.PI/180)*(360-a2);
@@ -70,7 +68,7 @@ function drawPie(c,p1,p2,r,a1,a2,clr) {
     c.fillStyle = clr;
     c.beginPath();
     c.arc(p1, p2, r, r2, r1, false); 
-    c.lineTo(p1,p2);
+    c.lineTo(p1, p2);
     c.closePath();
     c.fill();
     c.restore();
@@ -78,14 +76,14 @@ function drawPie(c,p1,p2,r,a1,a2,clr) {
 
 // Yes, the angle here is backwards from the unit circle angle in the
 // above functions (because this is for drawing time)
-function drawLine(c,p1x,p1y,d,angle,w,clr) {
+function drawLine(c, p1x, p1y, d, angle, w, clr) {
     var r = (Math.PI/180)*angle;
     c.save();
     c.strokeStyle = clr;
     c.lineWidth = w;
     c.beginPath();
     c.moveTo(p1x,p1y);
-    c.lineTo(p1x+d*Math.cos(r),p1y+d*Math.sin(r));
+    c.lineTo(p1x+d*Math.cos(r), p1y+d*Math.sin(r));
     c.stroke();
     c.restore();
 }
