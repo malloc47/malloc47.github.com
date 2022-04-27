721b9ec3263af377708a7346234c02887e26b562
Jarrell Waggoner <malloc47@gmail.com>
2022-04-26 22:20:40 -0400

    Far better way to handle switching CSS
    Now that I actually know a bit more javascript, this is far more
    elegant. Still a lot of room for improvement, but this is overall
    better.

--- _includes/check-time.js
+++ _includes/check-time.js
@@ -7,32 +7,9 @@ function check_time() {
     if(!window.theme_override) {
 	window.theme = lightp();
     }
-    if(window.theme && !document.getElementById('day-css')) {
-	var dLink = document.createElement('link')
-	dLink.href = '/css/day.css';
-	dLink.rel = 'stylesheet';
-	dLink.type = 'text/css';
-	dLink.id = 'day-css';
-	document.head.appendChild(dLink);
-	dLink = null;
-	// remove old header to prevent link tags building up if some
-	// poor soul leaves this site open for too long
-	if(document.getElementById('night-css')) {
-	    document.head.removeChild(document.getElementById('night-css'));
-	}
-    }
-    else if(!window.theme && !document.getElementById('night-css')) {
-	var dLink = document.createElement('link')
-	dLink.href = '/css/night.css';
-	dLink.rel = 'stylesheet';
-	dLink.type = 'text/css';
-	dLink.id = 'night-css';
-	document.head.appendChild(dLink);
-	dLink = null;
-	if(document.getElementById('day-css')) {
-	    document.head.removeChild(document.getElementById('day-css'));
-	}
-    }
+    document.getElementById('day-css').disabled = window.theme;
+    document.getElementById('night-css').disabled = !window.theme;
+
     drawClock();
 }
 
@@ -76,11 +53,4 @@ window.theme_override = cookie1;
 
 window.theme = window.theme_override ? cookie2 : lightp()
 
-// Doing this prevents loading the rest of the page before getting the
-// appropriate style (which would previously lead to flickering)
-if(window.theme) {
-    document.write('<link rel="stylesheet" id="day-css" href="/css/day.css">');
-}
-else {
-    document.write('<link rel="stylesheet" id="night-css" href="/css/night.css">');
-}
+check_time();
